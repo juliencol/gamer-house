f988383dca70096a5303e22afa89ca512d1bdba9
Julien Colombain <julien@colombain.com>
2021-05-10 14:37:06 +0200

    init backend api

---
+++ backend/src/server.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+app.use(bodyParser.json()).use(bodyParser.urlencoded({ extended: true }));
+
+
+app
+  .get('/', (req: Request, res: Response) =>
+    res.json({ message: 'Welcome to the GamerHouse API!' })
+  )
+  .listen(5000, () => {
+    console.log('Running GamerHouse API on http://localhost:5000');
+  });
+
+export default app;
