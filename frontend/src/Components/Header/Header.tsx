ec2da451f00dcb0cd336e0a54c00fc54b2c9064f
whitefacejunior <loic@honnore.com>
2021-05-31 09:29:48 +0200

    Revert "Frontend architecture"
    This reverts commit 432085c1edfd26826a074fe3643bad4cb27df60d.

--- frontend/src/Components/Header/Header.tsx
+++
@@ -1,7 +0,0 @@
-function Header() {
-  return (
-      <h1></h1>
-  );
-}
-
-export default Header;
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Homepage from "./Pages/Homepage";
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Homepage/>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
