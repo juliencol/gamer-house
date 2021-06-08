1fbd1fa28c15b7b1fef73f5e2a5a2ac268002c97
David Lamy <david.lamyv@gmail.com>
2021-06-08 15:15:26 +0200

    Post, post tag back, feed updated

---
+++ backend/src/post/post.types.ts
@@ -0,0 +1,8 @@
+import { IPost } from "../models/Post";
+
+export interface CreatePostArgs {
+  name: IPost["name"];
+  content: IPost["content"];
+  writer: IPost["writer"];
+  tags: IPost["tags"];
+}
