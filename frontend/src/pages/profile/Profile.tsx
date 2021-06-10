33e43b6735175e5f8630da9cc8bf7fc3d91d5d66
Sean <tang-sean.lim@isep.fr>
2021-06-10 20:25:18 +0200

    Added Carousel for Games Played

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -134,6 +134,7 @@ function Profile() {
     if (gamer?.following?.find((followedGamer) => followedGamer._id === id)) {
       return <Button disabled> Already Followed </Button>;
     }
+<<<<<<< HEAD
     return <Button onClick={() => followGamer(id)}>Follow</Button>;
   }
 
@@ -190,6 +191,41 @@ function Profile() {
           <h3>Carousel Photo</h3>
         </div>
       );
+=======
+
+
+    function createCarouselGame(){
+      const result : Array<JSX.Element> = [];
+      for(let i=0; i<3;i++){
+        result.push(
+          <div className="CarouselImg">
+            <h3>Carousel Photo</h3>
+          </div>
+        )
+      }
+      return result;
+    }
+    
+
+    const {Paragraph} = Typography;
+    const [editableStr, setEditableStr] = useState('This is an editable text.');
+    const [isModalVisible, setIsModalVisible] = useState(false);
+    const showModal = () => {
+        setIsModalVisible(true);
+    };
+    const handleOk = () => {
+        setIsModalVisible(false);
+      };
+    
+    const handleCancel = () => {
+    setIsModalVisible(false);
+    };
+
+    function getBase64(img:any, callback:any) {
+      const reader = new FileReader();
+      reader.addEventListener('load', () => callback(reader.result));
+      reader.readAsDataURL(img);
+>>>>>>> 250df06 (Added Carousel for Games Played)
     }
     return result;
   }
@@ -200,6 +236,7 @@ function Profile() {
     reader.readAsDataURL(img);
   }
 
+<<<<<<< HEAD
   return (
     <Layout>
       <Content>
@@ -208,6 +245,95 @@ function Profile() {
             <h1>Avatar</h1>
             <div>
               <Avatar size={64} icon={<UserOutlined />} />
+=======
+    const props = {
+        name: 'file',
+        action: 'https://www.mocky.io/v2/5cc8019d300000980a055e76',
+        headers: {
+          authorization: 'authorization-text',
+        },
+
+        
+         /* onChange(info:any) {
+          if (info.file.status !== 'uploading') {
+            console.log(info.file, info.fileList);
+            getBase64(info.file.originFileObj, imageUrl =>
+              this.setState({
+                imageUrl,
+                loading: false,
+              }),
+            );
+          }
+          if (info.file.status === 'done') {
+            message.success(`${info.file.name} file uploaded successfully`);
+          } else if (info.file.status === 'error') {
+            message.error(`${info.file.name} file upload failed.`);
+          }
+        }, */
+      };
+
+    return(
+        <div className="Profile">
+        <Layout>
+            <Content><p>Profile</p>
+
+            <div className="Content">
+
+                <div className="LeftColumm">
+                    <div className="Avatar">
+                      <p>Avatar</p>
+                      <div><Avatar size={64} icon={<UserOutlined />} /></div>
+                      <Upload {...props}>
+                          <Button icon={<UploadOutlined />}>Click to Upload</Button>
+                      </Upload>
+                    </div>
+                    <div className="RemoveGame">RemoveGame</div>
+                </div>
+
+                <div className="MainColumm">
+                    <div className="MainContent">
+                        <div className = "Description">Description 
+                        
+                          <div className="DescriptionTextWrapper">
+                            <Paragraph editable={{ onChange: setEditableStr }}>{editableStr}</Paragraph>
+                          </div>
+                        
+                        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                            <form action=""></form>
+                        </Modal>
+                        </div>
+                        
+                        <div className = "GamePlayed">I Play the Following games
+                        <div className="Button-wrapper">
+                            <Button type="dashed" size ="large" onClick={showModal}>
+                              Add Games
+                            </Button>
+                            <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+                            <form action=""></form>
+                            </Modal>
+                          </div>
+                        </div>
+
+                        <div className = "Event">Upcoming events
+                          <Carousel effect="scrollx">
+                            {createCarouselGame()}
+                          </Carousel>
+                        </div>
+                        
+                        <div className = "Follow">I Follow</div>
+                </div>
+                    
+                </div>
+
+                <div className="RightColumm">
+                    <div className="UserName">Username
+                    <p>{gamer?.pseudo}</p>
+                    </div>
+                    <div className="AddGame">Add Game +</div>
+                    <div className="AddFollow">Follow +</div>
+                </div>
+                    
+>>>>>>> 250df06 (Added Carousel for Games Played)
             </div>
             <Upload {...props}>
               <Button icon={<UploadOutlined />}>Click to Upload</Button>
