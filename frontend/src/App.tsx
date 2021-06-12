1ce9957325e29a5f007776a0571ba4c295a38016
whitefacejunior <loic@honnore.com>
2021-06-12 16:08:35 +0200

    Last FrontendCommit

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,11 +6,12 @@ import useFetch from './Components/use-fetch/useFetch';
 import { authReq } from './Services/AuthenticationServices';
 import Feed from './pages/feed/Feed';
 import Header from './Components/Header/Header';
+import Home from "../src/pages/Home/Home";
 
 function App() {
   const { data, setRequest, resetData } = useFetch<boolean>(
-    false,
-    authReq().isAuthenticated()
+      false,
+      authReq().isAuthenticated()
   );
   const [isAuthenticate, setIsAuthenticate] = useState(false);
 
@@ -26,33 +27,42 @@ function App() {
 
   if (!isAuthenticate) {
     return (
-      <Router>
-        <Switch>
-          <Route path="/">
-            <Authentication
-              setAuthentication={setIsAuthenticate}
-              refresh={() => setRequest(authReq().isAuthenticated())}
-            />
-          </Route>
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route path="/authentication" exact={true}>
+              <Route path="/">
+                <Header logOut={logOut} isAuthenticated={isAuthenticate}/>
+                <Authentication
+                    setAuthentication={setIsAuthenticate}
+                    refresh={() => setRequest(authReq().isAuthenticated())}
+                />
+              </Route>
+            </Route>
+            <Route path="/">
+              <Header logOut={logOut} isAuthenticated={isAuthenticate}/>
+              <Home/>
+            </Route>
+          </Switch>
+        </Router>
     );
   }
 
   return (
-    <Router>
-      <Header logOut={logOut} />
-      <Switch>
-        <Route exact={true} path="/">
-          <h1>You are now connected</h1>
-          <button onClick={() => logOut()}>Log out</button>
-        </Route>
-        <Route exact={true} path="/feed">
-          <Feed />
-        </Route>
-      </Switch>
-    </Router>
+      <Router>
+        <Header logOut={logOut} isAuthenticated={isAuthenticate}/>
+        <Switch>
+          <Route exact={true} path="/">
+            <h1>You are now connected</h1>
+            <button onClick={() => logOut()}>Log out</button>
+          </Route>
+          <Route exact={true} path="/feed">
+            <Feed/>
+          </Route>
+        </Switch>
+      </Router>
+
   );
+
 }
 
 export default App;
--- frontend/src/Components/Header/Header.tsx
+++ frontend/src/Components/Header/Header.tsx
@@ -4,62 +4,85 @@ import { useEffect, useState } from 'react';
 import './Header.css';
 import { Layout, Menu, Row, Col } from 'antd';
 import { Link } from 'react-router-dom';
+import src from '../../../src/Images/SocialGaming.png'
 
-function Header(props: { logOut: () => void }) {
+function Header(props: { logOut: () => void, isAuthenticated : boolean }) {
   const { data } = useFetch({ pseudo: '' }, userInfo().pseudo());
   const [pseudo, setPseudo] = useState('');
   useEffect(() => {
     setPseudo(data.pseudo);
   }, [data]);
 
-  return (
-    <Layout.Header className="background">
-      <Menu mode="horizontal" defaultSelectedKeys={['home']} className="menu">
-        <Menu.Item key="home">
-          <Link to="/" style={{ color: 'var(--white)', fontSize: '1em', bottom: '-3px' }}>
-            Home
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="feed">
-          <Link
-            to="/feed"
-            style={{ color: 'var(--white)', fontSize: '1em', bottom: '-3px' }}
-          >
-            Feed
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="profile">
-          <Link
-            to="/profile"
-            style={{ color: 'var(--white)', fontSize: '1em', bottom: '-3px' }}
-          >
-            Profile
-          </Link>
-        </Menu.Item>
-        <Menu.Item key="disconnect">
-          <Link
-            to="/"
-            style={{ color: 'var(--white)', fontSize: '1em', bottom: '-3px' }}
-            onClick={() => props.logOut()}
-          >
-            Log out
-          </Link>
-        </Menu.Item>
-      </Menu>
-      <Row align="middle" className="userRow">
-        <Col span={2}>
-          <img
-            className="avatar"
-            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReHQkNOzYqIg7yA0UfPI_ILNRbTvrgXflC6g&usqp=CAU"
-            alt=""
-          />
-        </Col>
-        <Col className="pseudo" span={21}>
-          {pseudo}
-        </Col>
-      </Row>
-    </Layout.Header>
-  );
+  if (props.isAuthenticated) {
+    return (
+        <Layout.Header className="background">
+          <Menu mode="horizontal" defaultSelectedKeys={['home']} className="menu">
+            <Menu.Item key="home">
+              <Link to="/" style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px'}}>
+                Home
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="feed">
+              <Link
+                  to="/feed"
+                  style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px'}}
+              >
+                Feed
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="profile">
+              <Link
+                  to="/profile"
+                  style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px'}}
+              >
+                Profile
+              </Link>
+            </Menu.Item>
+            <Menu.Item key="disconnect">
+              <Link
+                  to="/"
+                  style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px'}}
+                  onClick={() => props.logOut()}
+              >
+                Log out
+              </Link>
+            </Menu.Item>
+          </Menu>
+          <Row align="middle" className="userRow">
+            <Col span={2}>
+              <img
+                  className="avatar"
+                  src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReHQkNOzYqIg7yA0UfPI_ILNRbTvrgXflC6g&usqp=CAU"
+              />
+            </Col>
+            <Col className="pseudo" span={21}>
+              {pseudo}
+            </Col>
+          </Row>
+        </Layout.Header>
+    );
+  }
+
+    return(
+    <div className="navBar">
+      <Link to="/" style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px'}}>
+        <img src={src} alt = "logoSocialGaming" style={{width : "40px", marginLeft : "20px"}}/>
+      </Link>
+      <div className={"subNavBar"} >
+        <div style={{bottom: '-3px', marginRight : "70px", marginTop : "20px"}}>
+      <Link to="/about" style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px'}} className={window.location.pathname === "/about" ? "currentLink" : "linkDisconnected"}>
+        About
+      </Link>
+        </div>
+        <div style={{bottom: '-3px', marginRight : "70px", marginTop : "20px"}}>
+      <Link to="/authentication" style={{color: 'var(--white)', fontSize: '1em', bottom: '-3px', marginRight : "70px", marginTop : "20px"}} className={window.location.pathname !== "/authentication" ? "linkDisconnected" : "currentLink"}>
+        Sign In
+      </Link>
+        </div>
+      </div>
+    </div>
+    );
+
 }
 
 export default Header;
