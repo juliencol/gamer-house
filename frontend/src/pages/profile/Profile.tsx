cff02b39eea0e59910ea4a5dc4017de93edfcce3
David Lamy <david.lamyv@gmail.com>
2021-06-13 18:13:21 +0200

    Added GameAvatar everywhere

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -23,6 +23,7 @@ import GameServices from 'Services/GameServices';
 import { ChangeEvent, useEffect, useState } from 'react';
 import { Gamer, GameWithRank } from 'types/Gamer';
 import { Game } from 'types/Game';
+import GamerAvatar from 'Components/GamerAvatar/GamerAvatar';
 
 export type gameWithRank = 'gameId' | 'rank';
 
@@ -191,7 +192,7 @@ function Profile() {
   function displaySearchGamersResult() {
     return gamersSearchResult?.map((searchedGamer) => (
       <Row>
-        <img className="avatar" src={searchedGamer.profilePicture} />
+        <GamerAvatar avatarStyle="avatar" gamer={searchedGamer} />
         <div>
           <h1 style={{ margin: '0px', padding: '0px' }}>{searchedGamer.pseudo}</h1>
           <strong>{searchedGamer.statusMessage}</strong>
@@ -221,7 +222,7 @@ function Profile() {
   function displayGamers(gamers: [Gamer] | undefined) {
     return gamers?.map((gamer) => (
       <Col>
-        <img className="avatar" src={gamer.profilePicture} alt="avatar" />
+        <GamerAvatar avatarStyle="avatar" gamer={gamer} />
         <h1 style={{ margin: '0px' }}>{gamer.pseudo}</h1>
         {gamer.statusMessage}
       </Col>
