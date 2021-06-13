00140d13737982f1169d9c24323145bb190e8af7
whitefacejunior <loic@honnore.com>
2021-06-13 20:41:30 +0200

    English & Button

--- frontend/src/Components/Homepage/HomePageAbout/HomePageAbout.tsx
+++ frontend/src/Components/Homepage/HomePageAbout/HomePageAbout.tsx
@@ -10,15 +10,15 @@ function HomePageAbout(){
             <div className = "textContainer">
                 <div className = "textPart" >
                     <img src={src1} style={{marginTop : "-53px"}}/>
-                    <p style={{margin : "10px", textAlign : "center"}}>Jouez avec vos amis en quelques clics. Regardez si ils sont connectés, et à quoi ils jouent actuellement. Vous pourrez ainsi les rejoindre sur une partie avec une facilité déconcertante grâce à Gaming <span style={{color: "#9980fa"}}>House</span>.</p>
+                    <p style={{margin : "10px", textAlign : "center"}}>Play with your friends in a few clicks. See if they are online, and what they are currently playing. You can then join them in a game with ease thanks to Gaming <span style={{color: "#9980fa"}}>House</span>.</p>
                 </div>
                 <div className = "textPart">
                     <img src={src2}/>
-                    <p style={{margin : "10px", textAlign : "center"}}>L'union fait la force. Découvrez de nouvelles personnes et unissez vous afin de vous mener vers la victoire. Vous allez pouvoir dépasser vos limites en jouant avec de nouveaux gamers rencontrés grâce à Gaming <span style={{color: "#9980fa"}}>House</span>.</p>
+                    <p style={{margin : "10px", textAlign : "center"}}>There is strength in numbers. Discover new people and unite to lead you to victory. You will be able to exceed your limits by playing with new gamers you have met thanks to Gaming <span style={{color: "#9980fa"}}>House</span>.</p>
                 </div>
                 <div className = "textPart">
                     <img src={src3}/>
-                    <p style={{margin : "10px", textAlign : "center"}}>Créer une équipe compétitive afin de vous hisser plus rapidement à un niveau professionnel. Construisez une équipe pérenne grâce aux nombreuses fonctionnalités de groupes de Gaming <span style={{color: "#9980fa"}}>House</span>.</p>
+                    <p style={{margin : "10px", textAlign : "center"}}>Create a competitive team to get to a professional level faster. Build a sustainable team with the many group features of Gaming <span style={{color: "#9980fa"}}>House</span>.</p>
                 </div>
             </div>
         </div>
--- frontend/src/Components/Homepage/HomePageLanding/HomePageLanding.tsx
+++ frontend/src/Components/Homepage/HomePageLanding/HomePageLanding.tsx
@@ -12,8 +12,8 @@ function HomePageLanding(){
             <div className="homePageLandingTitleDiv">
                 <h1 className="homePageLandingTitle">Gaming <span style={{color: "#9980fa"}}>House</span></h1>
                 <p style={{marginTop : "-50px", fontSize : "2em"}}>Play.Together.</p>
-                {hover &&<Button shape="round" onMouseEnter={()=> sethover(!hover)} className="signInButton" style={{borderColor: "#9980fa", background: "none"}} type = "primary"><Link to="/authentication">Sing In</Link></Button>}
-                {!hover &&<Button shape="round" onMouseLeave={()=> sethover(!hover)} className="signInButton" style={{borderColor: "#9980fa", background: "#9980fa"} } type = "primary"><Link to="/authentication">Sing In</Link></Button>}
+                {hover &&<Button shape="round" onMouseEnter={()=> sethover(!hover)} className="signInButton" size = "large" style={{borderColor: "#9980fa", background: "none"}} type = "primary"><Link to="/authentication">Create an Account</Link></Button>}
+                {!hover &&<Button shape="round" onMouseLeave={()=> sethover(!hover)} className="signInButton" size = "large" style={{borderColor: "#9980fa", background: "#9980fa"} } type = "primary"><Link to="/authentication">Create an Account</Link></Button>}
             </div>
         </div>
     )
